0c6e882e4a8c38fdc18ef0ff4debb0d6bb45ea5f
Dylan Fontana <dyfontana@gmail.com>
2018-04-13 21:37:56 -0400

    Scraper done

--- Scraping/scraper.py
+++ Scraping/scraper.py
@@ -30,25 +30,52 @@ Run: scrapy runspider scraper.py
 """
 
 import scrapy
-from datetime import date as Date
+from datetime import date, datetime
 
 class NewsScraper(scrapy.Spider):
-  start_date = Date(2016,1,1)
-  end_date = Date(2017,12,31)
+  start_date = date(2016,1,1)
+  end_date = date(2017,12,31)
   current_date = start_date
-
   name = "abcnews_spider"
 
   def __init__(self):
     self.start_urls = ['http://www.abc.net.au/news/archive/?date='+self.getdate()]
 
   def parse(self, response):
-      TITLE_SELECTOR = 'h3 >.view-textlink > a ::text'
-      for headlines in response.css(TITLE_SELECTOR):
-          yield {
-            'titles': headlines.extract(),
-            'date': self.getdate()
-          }
+
+    TITLE_SELECTOR = 'h3 >.view-textlink > a ::text'
+    for headlines in response.css(TITLE_SELECTOR):
+        yield {
+          'date': self.getdate(),
+          'titles': headlines.extract()
+        }
+
+    NEXT_PAGE_SELECTOR = 'a[aria-label="Next page"] ::attr(href)'
+    NEXT_DATE_SELECTOR = '.archive-nav>a'
+
+    next_date_ele = response.css(NEXT_DATE_SELECTOR)
+
+    next_day = next_date_ele.css('::text').extract_first().strip(' \t\n\r')
+    next_date = next_date_ele.css('::attr(href)').extract_first()
+    next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+
+    if next_page:
+      yield scrapy.Request(
+        response.urljoin(next_page),
+        callback=self.parse
+      )
+    elif next_date:
+      self.current_date = datetime.strptime(next_day, '%a %d %b, %Y').date()
+
+      if self.current_date >= self.end_date:
+        # Stahp.
+        return
+
+      yield scrapy.Request(
+        response.urljoin(next_date),
+        callback=self.parse
+      )
+
 
   def getdate(self):
     return self.current_date.strftime('%Y-%m-%d')
