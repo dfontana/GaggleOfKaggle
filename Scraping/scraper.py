c77a1a3369869503bca682302e106636cbc80ada
Dylan Fontana <dyfontana@gmail.com>
2018-04-13 21:14:06 -0400

    Successfully scrapped a page

--- Scraping/scraper.py
+++ Scraping/scraper.py
@@ -1,5 +1,3 @@
-import scrapy
-
 """
 http://www.abc.net.au/news/archive/?date=2006-01-01&page=5
 
@@ -31,14 +29,26 @@ while(currentDate != startDate){
 Run: scrapy runspider scraper.py
 """
 
-class NewsScraper(scrapy.Spider):
-    name = "abcnews_spider"
-    start_urls = ['http://www.abc.net.au/news/archive/?date=2006-01-01']
+import scrapy
+from datetime import date as Date
 
-    def parse(self, response):
-        TITLE_SELECTOR = 'h3 >.view-textlink > a ::text'
-        for headlines in response.css(TITLE_SELECTOR):
-            yield {
-              'titles': headlines.extract(),
-              'date': 
-            }
+class NewsScraper(scrapy.Spider):
+  start_date = Date(2016,1,1)
+  end_date = Date(2017,12,31)
+  current_date = start_date
+
+  name = "abcnews_spider"
+
+  def __init__(self):
+    self.start_urls = ['http://www.abc.net.au/news/archive/?date='+self.getdate()]
+
+  def parse(self, response):
+      TITLE_SELECTOR = 'h3 >.view-textlink > a ::text'
+      for headlines in response.css(TITLE_SELECTOR):
+          yield {
+            'titles': headlines.extract(),
+            'date': self.getdate()
+          }
+
+  def getdate(self):
+    return self.current_date.strftime('%Y-%m-%d')
