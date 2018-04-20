381706e0f16ab07fdcc71b7b86a06b4aea5008d9
Dylan Fontana <dyfontana@gmail.com>
2018-04-19 23:27:51 -0400

    Renamed some scraping, working on making moods iterable

--- Sentiment/MoodsR.R
+++ Sentiment/MoodsR.R
@@ -2,11 +2,22 @@ library(SentimentAnalysis)
 library(readr)
 data = read_csv("C:/Users/Stephanie/DS3001/abcnews-date-text.csv")
 data = data.frame(data)
-data$publish_date = as.Date(as.character(data$publish_date), format = "%Y%m%d")
-yr03 = data[data$publish_date >= "2018-01-01" & data$publish_date < "2018-03-01",]
-headlines = yr03[,"headline_text"]
+data$date = as.Date(as.character(data$date), format = "%Y-%m-%d")
 
-sentiment = analyzeSentiment(headlines)
-directions = convertToDirection(sentiment$SentimentQDAP)
-write.table(directions, file="rsentiments.csv", sep=",", col.names = FALSE,row.names=FALSE, append=TRUE)
+start <- as.Date("01-08-14",format="%d-%m-%y")
+end   <- as.Date("08-09-14",format="%d-%m-%y")
+
+theDate <- start
+
+while (theDate <= end)
+{
+    yr03 = data[data$date >= "2018-01-01" & data$date < "2018-03-01",]
+    headlines = yr03[,"titles"]
+    sentiment = analyzeSentiment(headlines)
+    directions = convertToDirection(sentiment$SentimentQDAP)
+    write.table(directions, file="rsentiments.csv", sep=",", col.names = FALSE,row.names=FALSE, append=TRUE)
+    theDate <- theDate + 90
+}
+
+
 
